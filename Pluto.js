35daa9f326547e5cdfd67098792f632cd13694c2
Hussain Raza <52090345+Munkybooty@users.noreply.github.com>
2021-03-18 10:17:42 -0400

    Add files via upload
    4/25 Update when 3D visuals were implemented

--- Pluto.js
+++ Pluto.js
@@ -2,6 +2,6 @@
  * @fileoverview Pluto Object
  * @author Hussain Raza <hraza8@gatech.edu>
  * @author Isaac Naupa <iaguirre6@gatech.edu>
- * @version 1.1
+ * @version 1.2
  */
-var pluto = new Planet(1.3019e22,4.43682e12,7.37593e12,4743,1.1883e6,'#FFF1D5');
+var pluto = new Planet(1.3019e22,4.43682e12,7.37593e12,4743,1.1883e6,'https://i1124.photobucket.com/albums/l573/journeytotheunknown1/pluto.jpg');
